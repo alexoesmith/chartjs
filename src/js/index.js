628f1755100c2dd3c62dfc080ff69a6cc675d7df
Alex Smith <alex@gomedia.co>
2021-11-26 15:03:04 +0000

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+import "../scss/index.scss";
+import "../assets/logo.svg";
+
+import { scores, myChart, genres } from "./barChart";
+
+const checkBoxes = document.querySelectorAll(".checkbox");
+checkBoxes.forEach((checkBox) => {
+  checkBox.addEventListener("click", (e) => {
+    e.target.parentNode.classList[e.target.checked ? "add" : "remove"]("selected");
+
+    const genre = e.target.dataset.genre;
+    const checked = e.target.checked;
+
+    for (let i = 0; i < genres.length; i++) {
+      if (genres[i] == genre && checked) {
+        scores[i] += 10;
+      } else if (genres[i] == genre && !checked) {
+        scores[i] -= 10;
+      }
+    }
+
+    myChart.update();
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require("path");
+
+// Plugins
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/js/index.js",
+    chart: "./src/js/barChart.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].bundle.js",
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    open: true,
+    port: 3000,
+  },
+  devtool: "inline-source-map",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name]-[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      chunks: ["chart", "main"],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].min.css",
+    }),
+  ],
+  watch: true,
+};
